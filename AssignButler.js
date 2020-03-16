a0428f9a3ae2f7a7ee048c67f6d45d4e74605519
Anand Jain <anandjain.official@gmail.com>
2020-03-17 04:40:27 +0530

    clean optimized code

--- AssignButler.js
+++ AssignButler.js
@@ -8,32 +8,33 @@ const assignToButler = (taskReq, butlers) => {
                 remainingHours: butler.remainingHours - taskReq.hours
             }
             taskReq.assigned = true;                                      // set task assigned as true
-        } else if( i === butlers.length - 1 && !taskReq.assigned ) {
-            if(taskReq.hours <= 8) {                                      // else If task if <= 8 hours, just assign a new butler to it
-                butlers.push({
-                    requests: [taskReq.requestId],
-                    remainingHours: 8 - taskReq.hours
-                });
-            } else {                                                     // else if task > 8 hours, split it into multiple tasks 
-                const splicount = Math.ceil(taskReq.hours/8);            // and recursively call assignToButler function
-                let taskHrs = 0;
-                [...new Array(splicount)].forEach((el, i) => {
-                    let hr = 0;
-                    if(taskReq.hours - taskHrs > 8) {
-                        hr = 8;
-                        taskHrs += 8;
-                    } else {
-                        hr = taskReq.hours - taskHrs
-                    }
-                    assignToButler({
-                        ...taskReq,
-                        hours: hr
-                    }, butlers);
-                })
-            }
-            taskReq.assigned = true;
         }
-    })
+    });
+    if(!taskReq.assigned ) {
+        if(taskReq.hours <= 8) {                                      // else If task if <= 8 hours, just assign a new butler to it
+            butlers.push({
+                requests: [taskReq.requestId],
+                remainingHours: 8 - taskReq.hours
+            });
+        } else {                                                     // else if task > 8 hours, split it into multiple tasks 
+            const splicount = Math.ceil(taskReq.hours/8);            // and recursively call assignToButler function
+            let taskHrs = 0;
+            [...new Array(splicount)].forEach((el, i) => {
+                let hr = 0;
+                if(taskReq.hours - taskHrs > 8) {
+                    hr = 8;
+                    taskHrs += 8;
+                } else {
+                    hr = taskReq.hours - taskHrs
+                }
+                assignToButler({
+                    ...taskReq,
+                    hours: hr
+                }, butlers);
+            })
+        }
+        taskReq.assigned = true;
+    }
 }
 
 module.exports = function allocateAndReport(requests) {

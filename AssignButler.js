aae29a5691fa8eef83eb57a53c5820ac36cf97b0
Anand Jain <anandjain.official@gmail.com>
2020-03-16 16:45:57 +0530

    added comments

--- AssignButler.js
+++ AssignButler.js
@@ -2,20 +2,20 @@
 
 const assignToButler = (taskReq, butlers) => {
     butlers.forEach((butler, i) => {
-        if(butler.remainingHours >= taskReq.hours && !taskReq.assigned) {
+        if(butler.remainingHours >= taskReq.hours && !taskReq.assigned) { // Check if current butler in array has enough remaining time for non-assigned task
             butlers[i] = {
                 requests: [...butlers[i].requests, taskReq.requestId],
                 remainingHours: butler.remainingHours - taskReq.hours
             }
-            taskReq.assigned = true;
+            taskReq.assigned = true;                                      // set task assigned as true
         } else if( i === butlers.length - 1 && !taskReq.assigned ) {
-            if(taskReq.hours <= 8) {
+            if(taskReq.hours <= 8) {                                      // else If task if <= 8 hours, just assign a new butler to it
                 butlers.push({
                     requests: [taskReq.requestId],
                     remainingHours: 8 - taskReq.hours
-                })
-            } else {
-                const splicount = Math.ceil(taskReq.hours/8);
+                });
+            } else {                                                     // else if task > 8 hours, split it into multiple tasks 
+                const splicount = Math.ceil(taskReq.hours/8);            // and recursively call assignToButler function
                 let taskHrs = 0;
                 [...new Array(splicount)].forEach((el, i) => {
                     let hr = 0;
@@ -37,15 +37,15 @@ const assignToButler = (taskReq, butlers) => {
 }
 
 module.exports = function allocateAndReport(requests) {
-    const butlers = [{ requests: [], remainingHours: 8 }];
+    const butlers = [{ requests: [], remainingHours: 8 }]; // Initial butler Array
     const clientIds = [];
-    requests.forEach(taskReq => {
+    requests.forEach(taskReq => {                         // Looping through the requests
         clientIds.push(taskReq.clientId);
         taskReq.assigned = false;
-        assignToButler(taskReq, butlers);
+        assignToButler(taskReq, butlers);                 // Call assignToButler function.
     })
     return {
         butlers,
-        spreadClientIds: new Array(...new Set(clientIds))
+        spreadClientIds: new Array(...new Set(clientIds)) // To remove duplicates in clientIds Array
     }
 }
